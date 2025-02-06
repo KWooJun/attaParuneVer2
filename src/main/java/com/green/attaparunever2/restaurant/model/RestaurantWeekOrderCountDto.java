439b29c832af0f7c4a7989dd4c780f1a9f015e23
SugiSagong <jumoney1012@gmail.com>
2025-02-06 12:37:11 +0900

    feat: 식당 관리자 페이지 메인 주문 조회(예약완료, 예약)

---
+++ src/main/java/com/green/attaparunever2/restaurant/model/RestaurantWeekOrderCountDto.java
@@ -0,0 +1,7 @@
+package com.green.attaparunever2.restaurant.model;
+
+public class RestaurantWeekOrderCountDto {
+    private String date;
+    private int reservationOrders;
+    private int seatedOrders;
+}

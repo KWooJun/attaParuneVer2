19fbd388453598052ffedadb716077aea4dd8adb
SugiSagong <jumoney1012@gmail.com>
2025-02-06 13:06:51 +0900

    feat: 식당관리자 데시보드 조회 기능 작성

--- src/main/java/com/green/attaparunever2/restaurant/model/RestaurantWeekOrderCountDto.java
+++ src/main/java/com/green/attaparunever2/restaurant/model/RestaurantWeekOrderCountDto.java
@@ -1,5 +1,12 @@
 package com.green.attaparunever2.restaurant.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class RestaurantWeekOrderCountDto {
     private String date;
     private int reservationOrders;

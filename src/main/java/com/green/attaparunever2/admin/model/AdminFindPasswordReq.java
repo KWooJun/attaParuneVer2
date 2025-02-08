ac8fb446b67856c32dcf9ea129023cd216ad01f1
proregular <jumoney1012@gmail.com>
2025-02-08 14:01:20 +0900

    feat: 비밀번호 찾기 json 속성명 id로 매핑

--- src/main/java/com/green/attaparunever2/admin/model/AdminFindPasswordReq.java
+++ src/main/java/com/green/attaparunever2/admin/model/AdminFindPasswordReq.java
@@ -1,5 +1,7 @@
 package com.green.attaparunever2.admin.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,6 +10,9 @@ import lombok.ToString;
 @Setter
 @ToString
 public class AdminFindPasswordReq {
+    @JsonProperty("id")  // aid를 JSON에서 "id"로 매핑
+    @Schema(description = "아이디", example = "admin1", requiredMode = Schema.RequiredMode.REQUIRED)
     private String aid;
+    @Schema(description = "이메일", example = "test@gmail.com", requiredMode = Schema.RequiredMode.REQUIRED)
     private String email;
 }
